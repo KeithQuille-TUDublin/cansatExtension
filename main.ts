894a68aa323845ed83b74b58b35ab887ce92e93d
Keith Quille <72391746+KeithQuille-TUDublin@users.noreply.github.com>
2021-07-14 14:02:02 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /**
  *      CSinc Cansat extension
- *      V1.1
+ *      V1.2
  *      Developed by CSinc
  * 
  * 
@@ -66,26 +66,51 @@ namespace CanSat {
             externalTemp = pins.analogReadPin(AnalogPin.P0);
             metersAboveSeaLevel = -1;
 
+            try {
+                // For Version V2
+                let dataToSend: string = externalTemp + ";" +
+                    pressureMilliBars + ";" +
+                    metersAboveSeaLevel + ";" +
+                    input.temperature() + ";" +
+                    input.acceleration(Dimension.X) + ";" +
+                    input.acceleration(Dimension.Y) + ";" +
+                    input.acceleration(Dimension.Z) + ";" +
+                    input.acceleration(Dimension.Strength) + ";" +
+                    input.lightLevel() + ";" +
+                    input.rotation(Rotation.Pitch) + ";" +
+                    input.rotation(Rotation.Roll) + ";" +
+                    input.magneticForce(Dimension.Strength) + ";" +
+                    input.logoIsPressed() + ";" +
+                    input.soundLevel();
 
-            let dataToSend: string = externalTemp + ";" +
-                pressureMilliBars + ";" +
-                metersAboveSeaLevel + ";" +
-                input.temperature() + ";" +
-                input.acceleration(Dimension.X) + ";" +
-                input.acceleration(Dimension.Y) + ";" +
-                input.acceleration(Dimension.Z) + ";" +
-                input.acceleration(Dimension.Strength) + ";" +
-                input.lightLevel() + ";" +
-                input.rotation(Rotation.Pitch) + ";" +
-                input.rotation(Rotation.Roll) + ";" +
-                input.magneticForce(Dimension.Strength) + ";" +
-                input.logoIsPressed() + ";" +
-                input.soundLevel();
+
+                serial.writeLine(dataToSend);
+                showLed();
+                basic.pause(100);
+            }
+            catch (e) {
+                // For Version V1
+                let dataToSend: string = externalTemp + ";" +
+                    pressureMilliBars + ";" +
+                    metersAboveSeaLevel + ";" +
+                    input.temperature() + ";" +
+                    input.acceleration(Dimension.X) + ";" +
+                    input.acceleration(Dimension.Y) + ";" +
+                    input.acceleration(Dimension.Z) + ";" +
+                    input.acceleration(Dimension.Strength) + ";" +
+                    input.lightLevel() + ";" +
+                    input.rotation(Rotation.Pitch) + ";" +
+                    input.rotation(Rotation.Roll) + ";" +
+                    input.magneticForce(Dimension.Strength) + ";" +
+                    "NA_V1" + ";" +
+                    "NA_V1";
 
 
-            serial.writeLine(dataToSend);
-            showLed();
-            basic.pause(500);
+                serial.writeLine(dataToSend);
+                showLed();
+                basic.pause(100);
+            }
+
 
 
         }
