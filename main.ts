b6aa4295bc0a181949acf85deb498d09cd93aef8
Keith Quille <72391746+KeithQuille-TUDublin@users.noreply.github.com>
2021-07-14 14:07:59 +0100

    To work with V1 and V2

--- main.ts
+++ main.ts
@@ -56,7 +56,7 @@ namespace CanSat {
     }
 
     //% block
-    export function sendCansatData() {
+    export function sendCansatDataV1() {
 
         if (setUpSerial) {
             //pressureMilliBars = (pins.analogReadPin(AnalogPin.P0) / 1024) + 0.095) / 0.0009;
@@ -66,50 +66,27 @@ namespace CanSat {
             externalTemp = pins.analogReadPin(AnalogPin.P0);
             metersAboveSeaLevel = -1;
 
-            try {
-                // For Version V2
-                let dataToSend: string = externalTemp + ";" +
-                    pressureMilliBars + ";" +
-                    metersAboveSeaLevel + ";" +
-                    input.temperature() + ";" +
-                    input.acceleration(Dimension.X) + ";" +
-                    input.acceleration(Dimension.Y) + ";" +
-                    input.acceleration(Dimension.Z) + ";" +
-                    input.acceleration(Dimension.Strength) + ";" +
-                    input.lightLevel() + ";" +
-                    input.rotation(Rotation.Pitch) + ";" +
-                    input.rotation(Rotation.Roll) + ";" +
-                    input.magneticForce(Dimension.Strength) + ";" +
-                    input.logoIsPressed() + ";" +
-                    input.soundLevel();
+
+            let dataToSend: string = externalTemp + ";" +
+                pressureMilliBars + ";" +
+                metersAboveSeaLevel + ";" +
+                input.temperature() + ";" +
+                input.acceleration(Dimension.X) + ";" +
+                input.acceleration(Dimension.Y) + ";" +
+                input.acceleration(Dimension.Z) + ";" +
+                input.acceleration(Dimension.Strength) + ";" +
+                input.lightLevel() + ";" +
+                input.rotation(Rotation.Pitch) + ";" +
+                input.rotation(Rotation.Roll) + ";" +
+                input.magneticForce(Dimension.Strength) + ";" +
+                "NA_V1" + ";" +
+                "NA_V1";
 
 
-                serial.writeLine(dataToSend);
-                showLed();
-                basic.pause(100);
-            }
-            catch (e) {
-                // For Version V1
-                let dataToSend: string = externalTemp + ";" +
-                    pressureMilliBars + ";" +
-                    metersAboveSeaLevel + ";" +
-                    input.temperature() + ";" +
-                    input.acceleration(Dimension.X) + ";" +
-                    input.acceleration(Dimension.Y) + ";" +
-                    input.acceleration(Dimension.Z) + ";" +
-                    input.acceleration(Dimension.Strength) + ";" +
-                    input.lightLevel() + ";" +
-                    input.rotation(Rotation.Pitch) + ";" +
-                    input.rotation(Rotation.Roll) + ";" +
-                    input.magneticForce(Dimension.Strength) + ";" +
-                    "NA_V1" + ";" +
-                    "NA_V1";
+            serial.writeLine(dataToSend);
+            showLed();
+            basic.pause(100);
 
-
-                serial.writeLine(dataToSend);
-                showLed();
-                basic.pause(100);
-            }
 
 
 
@@ -120,6 +97,49 @@ namespace CanSat {
         }
 
     }
+
+
+    //% block
+    export function sendCansatDataV2() {
+
+        if (setUpSerial) {
+            //pressureMilliBars = (pins.analogReadPin(AnalogPin.P0) / 1024) + 0.095) / 0.0009;
+
+            // KQ more to do
+            pressureMilliBars = pins.analogReadPin(AnalogPin.P1);
+            externalTemp = pins.analogReadPin(AnalogPin.P0);
+            metersAboveSeaLevel = -1;
+
+
+            let dataToSend: string = externalTemp + ";" +
+                pressureMilliBars + ";" +
+                metersAboveSeaLevel + ";" +
+                input.temperature() + ";" +
+                input.acceleration(Dimension.X) + ";" +
+                input.acceleration(Dimension.Y) + ";" +
+                input.acceleration(Dimension.Z) + ";" +
+                input.acceleration(Dimension.Strength) + ";" +
+                input.lightLevel() + ";" +
+                input.rotation(Rotation.Pitch) + ";" +
+                input.rotation(Rotation.Roll) + ";" +
+                input.magneticForce(Dimension.Strength) + ";" +
+                input.logoIsPressed() + ";" +
+                input.soundLevel();
+
+
+            serial.writeLine(dataToSend);
+            showLed();
+            basic.pause(100);
+
+        }
+        else {
+            basic.showString("Error - Please set up transmission");
+
+        }
+
+    }
+
+
 
     function showLed() {
         if (sendingDot == 1) {
